6bab66a0aa7e16c263a4b6822b0d6d60fccc10cb
Valeriia <valeriia.chyrko@oa.edu.ua>
2025-03-10 15:39:48 +0200

    Updated links.

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
+    <a href="//vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+    <a href="developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
