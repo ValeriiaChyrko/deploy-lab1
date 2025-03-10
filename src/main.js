cbc29ac302cd4371f0d08a05074fa24b16a6a12b
Valeriia Chyrko <91199969+ValeriiaChyrko@users.noreply.github.com>
2025-03-10 15:44:41 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ document.querySelector('#app').innerHTML = `
     <a href="//vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
-    <a href="//developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+    <a href="developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
